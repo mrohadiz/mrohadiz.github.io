3b0a1c7a7bb4bd3b24f8a6141af3a7e3e2b42b31
M. Rohadiz <rohadi.m@outlook.com>
2026-07-12 01:00:07 +0000

    fix: generate search-index.json via post_write hook to bypass Liquid parsing

--- _plugins/search_index.rb
+++ _plugins/search_index.rb
@@ -1,38 +1,30 @@
 module Jekyll
-  class SearchIndexGenerator < Generator
-    safe true
-    priority :lowest
+  Jekyll::Hooks.register :site, :post_write do |site|
+    index = []
 
-    def generate(site)
-      index = []
-
-      site.posts.docs.each do |post|
-        index << {
-          'title' => post.data['title'] || post.title,
-          'url' => post.url,
-          'date' => post.date.strftime('%b %d, %Y'),
-          'tags' => (post.data['tags'] || []).join(', '),
-          'categories' => (post.data['categories'] || []).join(', '),
-          'excerpt' => post.data['excerpt'] ? post.data['excerpt'].strip.gsub(/<[^>]*>/, '')[0, 200] : '',
-          'content' => post.content.gsub(/<[^>]*>/, ' ').gsub(/\s+/, ' ')[0, 500]
-        }
-      end
-
-      site.pages.each do |page|
-        next if page.url =~ /\.(json|xml|txt|css|js|png|jpg|gif|svg)$/
-        index << {
-          'title' => page.data['title'] || page.title,
-          'url' => page.url,
-          'date' => page.data['date'] ? (page.data['date'].respond_to?(:strftime) ? page.data['date'].strftime('%b %d, %Y') : page.data['date'].to_s) : '',
-          'tags' => (page.data['tags'] || []).join(', '),
-          'content' => page.content.gsub(/<[^>]*>/, ' ').gsub(/\s+/, ' ')[0, 500]
-        }
-      end
-
-      file = PageWithoutAFile.new(site, site.source, '/', 'search-index.json')
-      file.content = index.to_json
-      file.data['layout'] = nil
-      site.pages << file
+    site.posts.docs.each do |post|
+      index << {
+        'title' => post.data['title'] || post.data['title'],
+        'url' => post.url,
+        'date' => post.date.strftime('%b %d, %Y'),
+        'tags' => (post.data['tags'] || []).join(', '),
+        'categories' => (post.data['categories'] || []).join(', '),
+        'excerpt' => post.data['excerpt'] ? post.data['excerpt'].strip.gsub(/<[^>]*>/, '')[0, 200] : '',
+        'content' => post.content.gsub(/<[^>]*>/, ' ').gsub(/\s+/, ' ')[0, 500]
+      }
     end
+
+    site.pages.each do |page|
+      next if page.url =~ /\.(json|xml|txt|css|js|png|jpg|gif|svg)$/
+      index << {
+        'title' => page.data['title'] || page.data['title'],
+        'url' => page.url,
+        'date' => page.data['date'] ? (page.data['date'].respond_to?(:strftime) ? page.data['date'].strftime('%b %d, %Y') : page.data['date'].to_s) : '',
+        'tags' => (page.data['tags'] || []).join(', '),
+        'content' => page.content.gsub(/<[^>]*>/, ' ').gsub(/\s+/, ' ')[0, 500]
+      }
+    end
+
+    File.write(File.join(site.dest, 'search-index.json'), index.to_json)
   end
 end
